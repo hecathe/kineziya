fa9c422dd4e4c2e8954ea0aca1f7153c248fefde
Kate <catherina1punk@gmail.com>
2020-09-22 19:04:49 +0300

    + adap almoust

--- source/scripts/swiper.js
+++ source/scripts/swiper.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
-  spaceBetween: 20,
+  //spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
