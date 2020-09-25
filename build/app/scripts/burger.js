6823e99190a803d872fe54fc486bb50566b7c647
Kate <catherina1punk@gmail.com>
2020-09-25 19:01:10 +0300

    + popups

--- build/app/scripts/burger.js
+++ build/app/scripts/burger.js
@@ -1,9 +1,27 @@
 var toggle = document.querySelector('.header__toggle');
 var headerNav = document.querySelector('.header__nav');
 var headerPage = document.querySelector('.header');
+var page = document.documentElement;
 
-toggle.addEventListener('click', function(event) {
+function toggleMenu(event) {
     toggle.classList.toggle('active');
     headerNav.classList.toggle('active');
     headerPage.classList.toggle('active');
+    page.classList.toggle('scroll-hidden');
+};
+
+toggle.addEventListener('click', function(e) {
+    e.stopPropagation();
+    toggleMenu();
+});
+
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == headerNav || headerNav.contains(target);
+    const its_toggle = target == toggle;
+    const headerNav_is_active = headerNav.classList.contains('active');
+    
+    if (!its_menu && !its_toggle && headerNav_is_active) {
+        toggleMenu();
+    }
 });
