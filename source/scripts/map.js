da34e2da87360fc195b9ede4c43f1e70fb09eeea
Kate <catherina1punk@gmail.com>
2020-10-02 15:58:04 +0300

    + map ctrl

--- source/scripts/map.js
+++ source/scripts/map.js
@@ -4,10 +4,12 @@ ymaps.ready(init);
     function init(){     
         myMap = new ymaps.Map ("map", {
             center: [59.9648238, 30.3240951],
-            behaviors: ['default', 'scrollZoom'],
+            //behaviors: ['default', 'scrollZoom'],
             zoom: 15,
             controls: ['smallMapDefaultSet']
         }),
+
+        myMap.behaviors.disable('scrollZoom');
 
         myPlacemark = new ymaps.Placemark([59.964823, 30.326270], {
             balloonContentHeader: "«Кинезия»",
@@ -21,4 +23,109 @@ ymaps.ready(init);
         });
         
         myMap.geoObjects.add(myPlacemark);
+
+        var ctrlKey = false;
+        var ctrlMessVisibility = false;
+        var timer;
+
+        function fadeIn(el) {
+            var opacity = 0.01;
+    
+            document.querySelector(el).style.display = 'block';
+    
+            /*var timer = setTimeout(function() {
+                if(opacity >= 1) {
+                    clearInterval(timer);
+                }
+                //ctrlMessVisibility = false;
+    
+                document.querySelector(el).style.opacity = opacity;
+                opacity += opacity*1;
+            }, 100);*/
+        };
+    
+        function fadeOut(el) {
+            var opacity = 1;
+    
+            document.querySelector(el).style.display = 'none';
+            /*var timer = setTimeout(function() {
+                if(opacity <= 0.1) {
+                    document.querySelector(el).style.display = 'none';
+                    clearInterval(timer);
+                }
+    
+                document.querySelector(el).style.opacity = opacity;
+                opacity -= opacity*1;
+            }, 100);*/
+        };
+
+        function offScroll() {
+            document.body.style.overflow = 'hidden';
+
+            myMap.onmouseout = function() {
+                document.body.style.overflow = 'scroll';
+            }
+        };
+
+        myMap.events.once('click', function() {
+            fadeOut('.contacts__map-ctrl');
+            myMap.behaviors
+                .enable(['scrollzoom'])
+                .enable(['multitouch']);
+        });
+
+        myMap.events.add(['wheel', 'mousedown'], function(e) {
+
+            if(e.get('type') == 'wheel') {
+                if(!ctrlKey) {
+                    fadeIn('.contacts__map-ctrl');
+                    ctrlMessVisibility = true;
+                    clearTimeout(timer);
+
+                    timer = setTimeout(function() {
+                        fadeOut('.contacts__map-ctrl');
+                        ctrlMessVisibility = false;
+                    }, 1500);
+                } else {
+                    fadeOut('.contacts__map-ctrl');
+                }
+            }
+
+            if(e.get('type') == 'mousedown' && ctrlMessVisibility) {
+                fadeOut('.contacts__map-ctrl');
+            }
+        });
+
+        var body = document.getElementsByTagName('body')[0];
+        body.onkeydown = callbackDown;
+        body.onkeyup = callbackUp;
+
+        function callbackDown(e){
+            if(e.keyCode === 17){
+                ctrlKey = true;
+                myMap.behaviors.enable(['scrollZoom']);
+            }
+        }
+
+        function callbackUp(e){
+            if(e.keyCode === 17){
+                ctrlKey = false;
+                myMap.behaviors.disable(['scrollZoom']);
+            }
+        }
+
+        /*document.addEventListener('keydown', function(event) {
+            if(e.keyCode === 17 && !ctrlKey) {
+                ctrlKey = true;
+                myMap.behaviors.enable('scrollZoom');
+            }
+        }),
+
+        document.addEventListener('keyup', function(event) {
+            if(e.keyCode === 17) {
+                myMap.behaviors.disable('scrollZoom');
+            }
+        });*/
     }
+
+    
